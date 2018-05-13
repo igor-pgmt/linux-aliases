eeea90d99c194f59120aeaf73b812fdc78584c15
Игорь <30924435+igor-pgmt@users.noreply.github.com>
2018-05-13 14:53:19 +0300

    added python alias

--- set_aliases.sh
+++ set_aliases.sh
@@ -23,3 +23,6 @@ USER_HOME=$(eval echo ~${SUDO_USER})
 echo "alias wadd=\"$USER_HOME/Scripts/siteadder.sh\"" >> /etc/bash.bashrc
 echo "alias yii2install=\"$USER_HOME/Scripts/yii2install.sh\"" >> /etc/bash.bashrc
 echo "alias tres=\"$USER_HOME/Scripts/tres.sh\"" >> /etc/bash.bashrc
+
+#python last version alias
+echo "alias python=/usr/local/bin/python3.6" >> /etc/bash.bashrc
